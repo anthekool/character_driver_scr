97a71e5cde4b5a19110e9335066e1c58fa9dc97b
anthekool <anupnsec@yahoo.in>
2016-02-25 13:19:31 -0500

    Added files via upload
    write file upadted

--- write_dev.c
+++ write_dev.c
@@ -8,7 +8,6 @@
 #include <linux/kernel.h>
 #include <linux/fs.h>
 
-//long copy_from_user(void *,const void *,long )
 extern ssize_t  write_dev(struct file *, const char *, size_t , loff_t *);
 long could_not_copy;
 struct scull_qset *lqset;
@@ -30,12 +29,12 @@ ssize_t write_dev(struct file *fops, const char *buf, size_t count, loff_t *f_po
 	ldev->qset_struc = (struct scull_qset *)kmalloc(sizeof(struct scull_qset),GFP_KERNEL);
 	lqset->data = (void **)kmalloc(sizeof(char *)*ldev->qset,GFP_KERNEL);
 	memset(lqset->data,0,sizeof(char *)*ldev->qset);
-/*	lqset->data[0]= (void **)kmalloc(ldev->quantum,GFP_KERNEL);
+	lqset->data[0]= (void **)kmalloc(ldev->quantum,GFP_KERNEL);
 	could_not_copy = copy_from_user(lqset->data[0],buf,4);
 	if(could_not_copy == 0)
 	{
 		printk(KERN_INFO "copy from user %ld\n",could_not_copy);
-	}*/
+	}
 	if(lqset->data != NULL)
 	{
 		printk(KERN_INFO "kmalloc successfully");
