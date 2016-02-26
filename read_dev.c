49bc67403f8eb59dc8d885fc43650221d3f72419
anthekool <anupnsec@yahoo.in>
2016-02-25 21:54:33 -0500

    Added files via upload
    read and write file updated

--- read_dev.c
+++ read_dev.c
@@ -9,7 +9,7 @@
 #include <linux/fs.h>
 
 extern ssize_t  read_dev(struct file *,char *, size_t , loff_t *);
-//long could_not_copy;
+long could_read_not_copy;
 //struct scull_qset *lqset;
 
 
@@ -17,13 +17,13 @@ ssize_t read_dev(struct file *fops, char *buf, size_t count, loff_t *f_pos)
 {
 
 
-//	struct scull_dev *ldev;
+	struct scull_dev *ldev;
 //	ldev->qset = 16;
 //	ldev->quantum = 4;
 //	struct scull_qset *lqset;
 //	struct scull_qset *lqset;
 //	long could_not_copy;
-//	ldev = fops->private_data; 
+	ldev = fops->private_data; 
 //	ldev->qset = 16;
 //	ldev->quantum = 4;
 
@@ -31,19 +31,21 @@ ssize_t read_dev(struct file *fops, char *buf, size_t count, loff_t *f_pos)
 //	ldev->qset_struc = (struct scull_qset *)kmalloc(sizeof(struct scull_qset),GFP_KERNEL);
 //	lqset->data = (void **)kmalloc(sizeof(char *)*ldev->qset,GFP_KERNEL);
 //	memset(lqset->data,0,sizeof(char *)*ldev->qset);
-/*	lqset->data[0]= (void **)kmalloc(ldev->quantum,GFP_KERNEL);
-	could_not_copy = copy_from_user(lqset->data[0],buf,4);
-	if(could_not_copy == 0)
+//	lqset->data[0]= (void **)kmalloc(ldev->quantum,GFP_KERNEL);
+	could_read_not_copy = copy_to_user(buf,ldev->qset_struc->data[0],4);
+	if(could_read_not_copy == 0)
 	{
-		printk(KERN_INFO "copy from user %ld\n",could_not_copy);
-	}*/
-/*	if(lqset->data != NULL)
-	{
-		printk(KERN_INFO "kmalloc successfully");
+		printk(KERN_INFO "copy from read user %s\n",buf);
 	}
 
+
+//	if(lqset->data != NULL)
+//	{
+//		printk(KERN_INFO "kmalloc successfully");
+//	}
+
 	else
-		printk(KERN_INFO "erro\n");*/
+		printk(KERN_INFO "read error %s\n",buf);
 
 	return 0;
 
--- user.c
+++ user.c
@@ -4,24 +4,32 @@
 #include<sys/types.h>
 #include<sys/stat.h>
 #include<unistd.h>
-char write_bytes[]="Embl";
-
+char write_bytes[]="anup";
+char read_bytes[4];
 int main()
 {
-	int fd,len,wr;
+	int fd,len,wr,rd;
 
-	fd = open("./node1",O_WRONLY);
+	fd = open("./node1",O_RDWR);
 	if(fd == -1)
 	printf("error in opening file\n");
 	else
 	printf("file open sucessfully\n");
 	len = strlen(write_bytes);
 	wr = write(fd,write_bytes,len);
-	if(wr == 4)
+	if(wr == 0)
 	printf("data write successfully\n");
 	else
-	printf("error in writing\n");		
-	//close(fd);
+	printf("error in writing\n");
+	rd = read(fd,read_bytes,len);
+	if(rd == 0)
+	printf("read successfully\n");
+	else
+	printf("error in reading file\n");
+	
+	printf("read and write num %d %d\n",rd,wr);
+	printf("read string is = %s %d\n",read_bytes,fd);	
+	close(fd);
 	return 0;
 
 }
